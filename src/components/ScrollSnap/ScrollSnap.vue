a6a989406f2529d78640f03532b854f2fcba7aca
kyliu <liukaiyuan@deltaphone.com.cn>
2021-11-19 16:21:00 +0800

    滚动贴合

--- src/components/ScrollSnap/ScrollSnap.vue
+++ src/components/ScrollSnap/ScrollSnap.vue
@@ -21,22 +21,30 @@
 export default {
   name: "ScrollSnap",
   data() {
-    return {}
+    return {};
   },
   mounted() {},
   methods: {
     onMouseWheel({ deltaY }) {
-      let boxDom = this.$refs["box2"]
-      let wrapDom = this.$refs["wrap"]
+      let boxDom = this.$refs["box2"];
+      let wrapDom = this.$refs["wrap"];
+      // 滚轮向下
       if (deltaY > 0) {
-        let top = Math.abs(wrapDom.offsetTop) + boxDom.clientHeight
-        const maxTop = wrapDom.offsetHeight - boxDom.clientHeight
-        top = top > maxTop ? maxTop : top
-        wrapDom.style.top = -top + "px"
+        let top = Math.abs(wrapDom.offsetTop) + boxDom.clientHeight;
+        const maxTop = wrapDom.offsetHeight - boxDom.clientHeight;
+        top = top > maxTop ? maxTop : top;
+        wrapDom.style.top = -top + "px";
+      }
+      // 滚轮向上
+      else {
+        let top = Math.abs(wrapDom.offsetTop) - boxDom.clientHeight;
+        const minTop = 0;
+        top = top < minTop ? minTop : top;
+        wrapDom.style.top = -top + "px";
       }
     },
   },
-}
+};
 </script>
 
 <style scoped lang="less">
